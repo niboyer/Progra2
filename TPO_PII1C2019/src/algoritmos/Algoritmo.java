5718d1a8ca0e492565abbe29b43c3165ef65b970
nicolasboyer <nicolasboyer@192.168.0.3>
2019-06-27 13:51:58 -0300

    Algoritmo

---
+++ TPO_PII1C2019/src/algoritmos/Algoritmo.java
@@ -0,0 +1,45 @@
+package algoritmos;
+
+import tdas.AgendaCitasTDA;
+import tdas.ColaPrioridadTDA;
+import tdas.ConjuntoTDA;
+
+public class Algoritmo implements IAlgoritmo {
+
+	@Override
+	public boolean disponible(AgendaCitasTDA agenda, String abogado, String fecha, String hora) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ConjuntoTDA masCitas(AgendaCitasTDA agenda, String fechaDesde, String fechaHasta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String abogadoUltimaVez(AgendaCitasTDA agenda, String cliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[][] obtenerCitas(AgendaCitasTDA agenda, String abogado, String fecha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[][] conQuienSeReunio(AgendaCitasTDA agenda, String cliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ColaPrioridadTDA libresTotal(AgendaCitasTDA agenda, String fecha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
